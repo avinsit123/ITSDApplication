38ceba5cb90c59389acb6dcd502861067bd15ac1
avinsit123 <s.avinash.it.17@nsit.net.in>
2022-01-08 11:19:20 +0530

    Added Spring Files

---
+++ src/main/java/com/io/itsd/ItsdApplication.java
@@ -0,0 +1,13 @@
+package com.io.itsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/itsd/ItsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.itsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
