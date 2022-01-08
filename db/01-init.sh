9d04a86b74fc849cce5991985381d150b9728eb1
avinsit123 <s.avinash.it.17@nsit.net.in>
2022-01-08 20:52:52 +0530

    Added HibernateConfigs

---
+++ db/01-init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
+  \connect $APP_DB_NAME $APP_DB_USER
+  BEGIN;
+    CREATE TABLE IF NOT EXISTS request (
+    id CHAR(100) UNIQUE NOT NULL PRIMARY KEY,
+    assignee_name CHAR(100),
+    status CHAR(100),
+    title CHAR(100),
+    description CHAR(100),
+    created_at CHAR(100),
+    updated_at CHAR(100),
+    customer CHAR(100));
+  COMMIT;
+EOSQL
---
+++ src/main/java/com/io/itsd/config/HibernateConfig.java
@@ -0,0 +1,49 @@
+package com.io.itsd.config;
+
+import com.io.itsd.model.Request;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:hibernate.properties")
+public class HibernateConfig {
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
+    @Value("${hibernate.connection.driver_class}")
+    private String driverClass;
+
+    @Value("${hibernate.connection.url}")
+    private String connectionUrl;
+
+    @Value("${hibernate.connection.username}")
+    private String username;
+
+    @Value("${hibernate.connection.password}")
+    private String password;
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", dialect);
+        properties.setProperty("hibernate.connection.driver_class", driverClass);
+        properties.setProperty("hibernate.connection.url", connectionUrl);
+        properties.setProperty("hibernate.connection.username", username);
+        properties.setProperty("hibernate.connection.password", password);
+
+        org.hibernate.cfg.Configuration cfg = new org.hibernate.cfg.Configuration();
+
+        // todo: Add All Entity Classes to annotated class.
+        SessionFactory factory = cfg.setProperties(properties)
+                .addAnnotatedClass(Request.class)
+                .buildSessionFactory();
+        return factory;
+    }
+
+}
